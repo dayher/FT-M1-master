5c2f2f3acf5e924449366ed8266251bafb1c3f7c
Dayher Marchan <dayhermarchan@gmail.com>
2022-05-05 21:51:01 -0300

    Algoritmos II

--- FT-M1-master/07-Algoritmos-II/homework/homework.js
+++ FT-M1-master/07-Algoritmos-II/homework/homework.js
@@ -7,34 +7,20 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-  if(array.legth <= 1) return array;
- /* 
-  if(array.length == 2 ) {
-    if (array[0]>array[1]) {
-      let retorno = []
-      retorno[0]=array[1];
-      retorno[1]=array[0];
-      return retorno;
-    } 
-    
-  }
-  */
-  let pivote = Math.floor((Math.random())*(array.length-1));
+  if(array.length <= 1) return array;
 
-  let arrIzq = [];
-  let arrDer = [];
+  let pivot = Math.floor(Math.random() * array.length),
+  smaller = [],
+  bigger = [],
+  equals = [];
 
   for (let i = 0; i < array.length; i++) {
-
-    if(array[i]<=array[pivote]){
-      arrDer.push(array[i]);
-    } else {
-      arrIzq.push(array[i]);
-    }
-
+    if (array[i] < array[pivot]) smaller.push(array[i]);
+    else if (array[i] > array[pivot]) bigger.push(array[i]);
+    else equals.push(array[i]);
   }
 
-return quickSort(arrIzq).concat(quickSort(arrDer));
+  return [...quickSort(smaller), ...equals, ...quickSort(bigger)];
 
 }
 
@@ -43,8 +29,34 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
+
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
 
 }
+
+function merge(arr1, arr2) {
+  let idx1 = 0,
+    idx2 = 0,
+    final = [];
+    while (idx1 < arr1.length && idx2 < arr2.length) {
+      if (arr1[idx1] < arr2[idx2]) {
+        final.push(arr1[idx1]);
+        idx1++;
+      } else {
+        final.push(arr2[idx2]);
+        idx2++;
+      }
+    }
+
+    return final.concat(arr1.slice(idx1)).concat(arr2.slice(idx2));
+
+}
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
